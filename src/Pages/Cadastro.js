354d3fabf12eb1e02ea24dc7de94bc7bab3988c1
garrcastro <garrcastro@gmail.com>
2023-04-24 21:43:46 -0300

    aula feita

--- src/Pages/Cadastro.js
+++ src/Pages/Cadastro.js
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { handleHome } from "../Router/cordinator";
 import styled from "styled-components";
 
-export default function Cadastro (props){
+export default function Cadastro ({frutas, setFrutas}){
+  
   const[formulario, setFormulario]=useState({name:"", url:"", price:""})
   const navigate = useNavigate();
 
@@ -14,7 +15,15 @@ export default function Cadastro (props){
   const handleClick = (event)=>{
     event.preventDefault()
     console.log(formulario);
-  }
+    setFrutas([...frutas,
+      {
+        ...formulario,
+        id: frutas.length + 1
+      }
+      ])
+    }
+  
+ 
  
  
   return(
